8bb49d210c123874a0f3828dfb3b00cf81327cd8
Aditee Verma <31782026+averma74@users.noreply.github.com>
2018-06-01 19:12:35 -0400

    Add files via upload

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG={
+  apiKey: "AIzaSyDUTftYKnYgypk2txA_Jo9312x5yrydxBc",
+  authDomain: "shoppinglist-2cb96.firebaseapp.com",
+  databaseURL: "https://shoppinglist-2cb96.firebaseio.com",
+  projectId: "shoppinglist-2cb96",
+  storageBucket: "",
+  messagingSenderId: "504688329301"
+};
